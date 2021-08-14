a520a94e583cf93171a670d420f8f9185da3163b
Teerasej <teerasej@gmail.com>
2021-08-14 23:44:00 +0700

    อัพเดต class จาก code ที่ได้จาก quicktype - เอา json ตัวอย่างไปวางใน quicktype - เอา code ที่ได้มาวาง ใน news_data.dart - เพิ่ม required keyword ใน constructor

--- lib/news_data.dart
+++ lib/news_data.dart
@@ -1,5 +1,30 @@
-class NewsData {
-  String content = "...";
+// To parse this JSON data, do
+//
+//     final newsData = newsDataFromJson(jsonString);
 
-  NewsData(this.content);
+import 'dart:convert';
+
+class NewsData {
+  NewsData({
+    required this.id,
+    required this.content,
+  });
+
+  String id;
+  String content;
+
+  factory NewsData.fromRawJson(String str) =>
+      NewsData.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory NewsData.fromJson(Map<String, dynamic> json) => NewsData(
+        id: json["_id"] == null ? null : json["_id"],
+        content: json["content"] == null ? null : json["content"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id == null ? null : id,
+        "content": content == null ? null : content,
+      };
 }
