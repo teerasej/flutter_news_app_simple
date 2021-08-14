baa94c3ebec719fb4c945e1338e944f56178703a
Teerasej <teerasej@gmail.com>
2021-08-14 23:19:55 +0700

    เพิ่มแบบฟอร์มและปุ่ม

--- lib/create_news_page.dart
+++ lib/create_news_page.dart
@@ -14,6 +14,24 @@ class _CreateNewsPageState extends State<CreateNewsPage> {
       appBar: AppBar(
         title: Text('Create News'),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('เนื้อหาใหม่'),
+              TextFormField(),
+              ElevatedButton(
+                onPressed: () {
+                  print('do something');
+                },
+                child: Text('เพิ่ม'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
