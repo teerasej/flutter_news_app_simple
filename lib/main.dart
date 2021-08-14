8aa21fc4d775025d27562583c7ff0aeb66be5f3c
Teerasej <teerasej@gmail.com>
2021-08-14 22:54:55 +0700

    สร้าง MyApp widget และย้าย Material App ไปไว้ด้านใน

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Flutter Demo',
       home: Text('oh, hello'),
-    ),
-  );
+    );
+  }
 }
