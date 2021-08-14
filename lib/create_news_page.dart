2f09c6f6f003af635ec128f43ef7b1f0d3cd2315
Teerasej <teerasej@gmail.com>
2021-08-14 23:07:29 +0700

    สร้างหน้า Create News Page

---
+++ lib/create_news_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateNewsPage extends StatefulWidget {
+  const CreateNewsPage({Key? key}) : super(key: key);
+
+  @override
+  _CreateNewsPageState createState() => _CreateNewsPageState();
+}
+
+class _CreateNewsPageState extends State<CreateNewsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
