7f2068c6f9cc57424ba9f960ba3250537f67f88f
Teerasej <teerasej@gmail.com>
2021-08-15 13:14:16 +0700

    ส่งข้อมูลไปบันทึกที่ web api - สร้างโครงสร้างของ JSON - ใช้ jsonEncode function ในการแปลงเป็นข้อความ json - กำหนด url ของ web api ในการส่ง request - ส่ง request แบบ POST โดยกำหนด header เป็น json - แนบข้อความ json ไปกับ body - เช็คสถานะตอบกลับ ถ้าเป็น 200 ถือว่าโอเค เปิดกลับหน้าแรกด้วย pop() - ถ้าไม่ใช่ก็แสดงข้อความ

--- lib/create_news_page.dart
+++ lib/create_news_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_app/news_data.dart';
 
 class CreateNewsPage extends StatefulWidget {
   const CreateNewsPage({Key? key}) : super(key: key);
@@ -32,8 +36,29 @@ class _CreateNewsPageState extends State<CreateNewsPage> {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
+                  onPressed: () async {
                     var newsContent = newsContentController.text;
                     print(newsContent);
+
+                    var newsData = {"content": newsContent};
+                    var json = jsonEncode(newsData);
+
+                    var url =
+                        Uri.parse('http://c8d8b4dc6a7d.ngrok.io/news/create');
+
+                    var response = await http.post(
+                      url,
+                      headers: {"Content-Type": "application/json"},
+                      body: json,
+                    );
+
+                    if (response.statusCode == 200) {
+                      Navigator.pop(context);
+                    } else {
+                      print(response.body);
+                    }
+
+                    //
                   },
                   child: Text('เพิ่ม'),
                 ),
