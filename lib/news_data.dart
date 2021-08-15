b4657d39531c4462f433fe14dcd6a36016af2b47
Teerasej <teerasej@gmail.com>
2021-08-15 12:03:37 +0700

    ใช้ตัวแปลง json แบบแยก method

--- lib/news_data.dart
+++ lib/news_data.dart
@@ -2,7 +2,14 @@
 //
 //     final newsData = newsDataFromJson(jsonString);
 
+import 'package:meta/meta.dart';
 import 'dart:convert';
+
+List<NewsData> newsDataFromJson(String str) =>
+    List<NewsData>.from(json.decode(str).map((x) => NewsData.fromJson(x)));
+
+String newsDataToJson(List<NewsData> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class NewsData {
   NewsData({
@@ -13,18 +20,13 @@ class NewsData {
   String id;
   String content;
 
-  factory NewsData.fromRawJson(String str) =>
-      NewsData.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
   factory NewsData.fromJson(Map<String, dynamic> json) => NewsData(
-        id: json["_id"] == null ? null : json["_id"],
-        content: json["content"] == null ? null : json["content"],
+        id: json["_id"],
+        content: json["content"],
       );
 
   Map<String, dynamic> toJson() => {
-        "_id": id == null ? null : id,
-        "content": content == null ? null : content,
+        "_id": id,
+        "content": content,
       };
 }
