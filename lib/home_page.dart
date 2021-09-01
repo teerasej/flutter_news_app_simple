1510b42cd9d09e6b1c4d3c1b8667df6132b9020d
Teerasej <teerasej@gmail.com>
2021-09-01 21:26:37 +0700

    clear for workshop

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,75 +13,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //List<String> newsItems = ['abc', 'def', 'ghi'];
-
-  Uri webApi = Uri.parse('http://c8d8b4dc6a7d.ngrok.io/news');
-
-  // List<NewsData> newsItems = [
-  //   NewsData(id: '1', content: 'abc'),
-  //   NewsData(id: '2', content: 'def'),
-  //   NewsData(id: '3', content: 'ghi')
-  // ];
+  // Uri webApi = Uri.parse('http://c8d8b4dc6a7d.ngrok.io/news');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('News'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('click');
+      appBar: AppBar(
+        title: Text('News'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('click');
 
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) {
-                return CreateNewsPage();
-              }),
-            ).then((value) {
-              setState(() {});
-            });
-          },
-        ),
-        body: FutureBuilder(
-          future: http.get(webApi),
-          builder: (BuildContext context, AsyncSnapshot<Response> snapshot) {
-            if (snapshot.hasError) {
-              return Text('opps... ${snapshot.error.toString()}');
-            }
-
-            if (snapshot.connectionState == ConnectionState.done) {
-              var jsonFromAPI = snapshot.data?.body as String;
-              var newsItems = newsDataFromJson(jsonFromAPI);
-
-              return ListView.separated(
-                itemCount: newsItems.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(newsItems[index].content),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return Divider();
-                },
-              );
-            }
-
-            return Center(child: CircularProgressIndicator());
-          },
-        )
-        // body: ListView.separated(
-        //   itemCount: newsItems.length,
-        //   itemBuilder: (BuildContext context, int index) {
-        //     return ListTile(
-        //       title: Text(newsItems[index].content),
-        //     );
-        //   },
-        //   separatorBuilder: (context, index) {
-        //     return Divider();
-        //   },
-        // )
-        );
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return CreateNewsPage();
+            }),
+          ).then((value) {
+            setState(() {});
+          });
+        },
+      ),
+      body: Container(),
+    );
   }
 }
